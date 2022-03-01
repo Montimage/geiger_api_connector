bf50c1aa785d87b7d75c23707708b8c72eb9a820
Luong NGUYEN <luongnv89@gmail.com>
2022-03-01 11:36:48 +0100

    add a dummy test

--- test/geiger_api_connector_test.dart
+++ test/geiger_api_connector_test.dart
@@ -1,23 +1,9 @@
 // import 'package:flutter/services.dart';
-// import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_test/flutter_test.dart';
 // import 'package:geiger_api_connector/geiger_api_connector.dart';
 
-// void main() {
-//   const MethodChannel channel = MethodChannel('geiger_api_connector');
-
-//   TestWidgetsFlutterBinding.ensureInitialized();
-
-//   setUp(() {
-//     channel.setMockMethodCallHandler((MethodCall methodCall) async {
-//       return '42';
-//     });
-//   });
-
-//   tearDown(() {
-//     channel.setMockMethodCallHandler(null);
-//   });
-
-//   test('getPlatformVersion', () async {
-//     expect(await GeigerApiConnector.platformVersion, '42');
-//   });
-// }
+void main() {
+  test('dummy Test', () async {
+    expect(true, true);
+  });
+}
