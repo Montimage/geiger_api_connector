500cd88be01c2d79877041aabd32aaff72596af1
Nosa Daniel <nosadaniel02@gmail.com>
2022-07-15 18:06:22 +0200

    added geiger service,  uncomment kotlin code

--- example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
@@ -2,17 +2,17 @@ package com.montimage.master
 
 import io.flutter.embedding.android.FlutterActivity
 
-// import android.content.Context
-// import eu.cybergeiger.communication.GeigerService
-// import io.flutter.embedding.engine.FlutterEngine
+ import android.content.Context
+ import eu.cybergeiger.communication.GeigerService
+ import io.flutter.embedding.engine.FlutterEngine
 
 class MainActivity: FlutterActivity() {
-/*    override fun provideFlutterEngine(context: Context): FlutterEngine {
+    override fun provideFlutterEngine(context: Context): FlutterEngine {
         return GeigerService.claimEngine(context)
     }
 
     override fun cleanUpFlutterEngine(flutterEngine: FlutterEngine) {
         GeigerService.releaseEngine()
     }
-*/
+
 }
